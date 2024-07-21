8e44b7a14e2e8c85b022a739e7a70e4fbfec8a12
bryson80 <chitayibraison93@gmail.com>
2024-07-21 19:30:05 +0300

    script.js

--- script.js
+++ script.js
@@ -71,6 +71,34 @@ document.addEventListener('DOMContentLoaded', function() {
             message.style.color = 'red';
         }
     });
+    // Chart.js setup
+    const ctx = document.getElementById('myChart').getContext('2d');
+    const myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [{
+                labels: '# of Expenses',
+                data: [15, 20, 5, 8, 1, 5, 10],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)'
+                ],
+                borderWidth: 1
+            }]
+        }
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
 });
 
 
